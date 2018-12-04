8da7fd5d49fa915ace49eae5cc58c49474d0ba25
smael.tanrhori <smael.tanrhori@FR-L3356930.groupinfra.com>
2018-12-04 11:19:46 +0100

    database working

--- src/main/java/com/udev/lightning/LightningApplication.java
+++ src/main/java/com/udev/lightning/LightningApplication.java
@@ -2,7 +2,11 @@ package com.udev.lightning;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+@EnableJpaRepositories("com.udev.lightning.repo") 
+@EntityScan("com.udev.lightning.beans")
 @SpringBootApplication
 public class LightningApplication {
 
